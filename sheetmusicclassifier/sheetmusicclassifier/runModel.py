488b85fd30f2e765a1cd236577e238aa2fe423fd
jdchart <jacob.dchart@gmail.com>
2023-06-21 15:38:08 +0200

    stfu

--- sheetmusicclassifier/sheetmusicclassifier/runModel.py
+++ sheetmusicclassifier/sheetmusicclassifier/runModel.py
@@ -33,12 +33,9 @@ def classify_images(model_path, image_paths):
 
         # Classification
         scores = classifier.predict(numpy.array([normalized_input_image])).flatten()
-        print(" Class scores: {0}".format(numpy.array2string(scores, formatter={'float_kind': lambda x: "%0.2f" % x})))
         class_with_highest_probability = numpy.where(scores == scores.max())[0][0]
 
         class_names = ['other', 'score']
-        print(" Image is most likely: {0} (certainty: {1:0.2f})".format(class_names[class_with_highest_probability],
-                                                                        scores[class_with_highest_probability]))
         
         returnData["images"].append({
             "path" : img,
