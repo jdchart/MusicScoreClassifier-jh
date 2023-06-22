58d8301123eaf812986db869c4e7b6c6b6b2de62
jdchart <jacob.dchart@gmail.com>
2023-06-22 12:50:22 +0200

    can run on single image

--- sheetmusicclassifier/sheetmusicclassifier/main.py
+++ sheetmusicclassifier/sheetmusicclassifier/main.py
@@ -1,4 +1,7 @@
 from .runModel import *
 
 def process(model, toProcess):
-    return classify_images(model, toProcess)
+    if type(toProcess) == "str":
+        return classify_image(model, toProcess)
+    else:
+        return classify_images(model, toProcess)
--- sheetmusicclassifier/sheetmusicclassifier/runModel.py
+++ sheetmusicclassifier/sheetmusicclassifier/runModel.py
@@ -47,5 +47,32 @@ def classify_images(model_path, image_paths):
 
     return returnData
 
+def classify_image(model_path, image_path):
+    # Load classifier
+    classifier = keras.models.load_model(model_path)
+    #classifier.summary()
+    input_shape = classifier.input_shape[1:4]
+
+    # Load image
+    input_image = imageio.imread(image_path, mode="RGB")
+
+    # Image preprocessing
+    normalized_input_image = resize(input_image, output_shape=input_shape, preserve_range=True)
+    normalized_input_image = normalized_input_image.astype(numpy.float32)
+
+    # Classification
+    scores = classifier.predict(numpy.array([normalized_input_image])).flatten()
+    class_with_highest_probability = numpy.where(scores == scores.max())[0][0]
+
+    class_names = ['other', 'score']
+    
+    return {
+        "path" : image_path,
+        "scores" : [to_python_float(scores[0]), to_python_float(scores[1])],
+        "highest_prob" : to_python_float(class_with_highest_probability),
+        "class" : class_names[class_with_highest_probability],
+        "certainty" : to_python_float(scores[class_with_highest_probability])
+    }
+
 def to_python_float(npInput):
     return round(npInput.item(), 4)
