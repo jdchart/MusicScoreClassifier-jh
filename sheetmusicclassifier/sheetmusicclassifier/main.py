5e3fcd18a2c6533d1a17b09009783deaca7dded3
jdchart <jacob.dchart@gmail.com>
2023-06-23 10:56:15 +0200

    type checking fix

--- sheetmusicclassifier/sheetmusicclassifier/main.py
+++ sheetmusicclassifier/sheetmusicclassifier/main.py
@@ -1,7 +1,7 @@
 from .runModel import *
 
 def process(model, toProcess):
-    if type(toProcess) == "str":
+    if isinstance(toProcess, str):
         return classify_image(model, toProcess)
     else:
         return classify_images(model, toProcess)
