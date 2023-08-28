a729f361049f7d8299d66678c9a546afe16bba1a
jdchart <jacob.dchart@gmail.com>
2023-08-28 12:27:58 +0200

    dump

--- sheetmusicclassifier/build/lib/sheetmusicclassifier/runModel.py
+++ sheetmusicclassifier/build/lib/sheetmusicclassifier/runModel.py
@@ -33,12 +33,9 @@ def classify_images(model_path, image_paths):
 
         # Classification
         scores = classifier.predict(numpy.array([normalized_input_image])).flatten()
-        print(" Class scores: {0}".format(numpy.array2string(scores, formatter={'float_kind': lambda x: "%0.2f" % x})))
         class_with_highest_probability = numpy.where(scores == scores.max())[0][0]
 
         class_names = ['other', 'score']
-        print(" Image is most likely: {0} (certainty: {1:0.2f})".format(class_names[class_with_highest_probability],
-                                                                        scores[class_with_highest_probability]))
         
         returnData["images"].append({
             "path" : img,
@@ -49,6 +46,33 @@ def classify_images(model_path, image_paths):
         })
 
     return returnData
+
+def classify_image(model_path, image_path):
+    # Load classifier
+    classifier = keras.models.load_model(model_path)
+    #classifier.summary()
+    input_shape = classifier.input_shape[1:4]
+
+    # Load image
+    input_image = imageio.imread(image_path, mode="RGB")
+
+    # Image preprocessing
+    normalized_input_image = resize(input_image, output_shape=input_shape, preserve_range=True)
+    normalized_input_image = normalized_input_image.astype(numpy.float32)
+
+    # Classification
+    scores = classifier.predict(numpy.array([normalized_input_image])).flatten()
+    class_with_highest_probability = numpy.where(scores == scores.max())[0][0]
+
+    class_names = ['other', 'score']
+    
+    return {
+        "path" : image_path,
+        "scores" : [to_python_float(scores[0]), to_python_float(scores[1])],
+        "highest_prob" : to_python_float(class_with_highest_probability),
+        "class" : class_names[class_with_highest_probability],
+        "certainty" : to_python_float(scores[class_with_highest_probability])
+    }
 
 def to_python_float(npInput):
     return round(npInput.item(), 4)
